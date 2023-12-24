f3b6e5107b0ccbfb5d8931f73b9d842796cfd428
Kalin Johnson <kalin.m.johnson@gmail.com>
2023-12-24 16:52:34 -0600

    Update Credentials.cs

--- Kuscotopia/Common/Credentials.cs
+++ Kuscotopia/Common/Credentials.cs
@@ -2,11 +2,11 @@
 
 public class Credentials
 {
-    public const string QueueKeyId = "AKIAYJPIYQ3CQU7VZCGC";
+    public const string QueueKeyId = "RemovedForSecurityReasons";
 
-    public const string QueueKey = "vJcQF7asN7+mOqRrW4PkS5aOCliUT2NRYJWB7uxC";
+    public const string QueueKey = "RemovedForSecurityReasons";
 
-    public const string QueueUrl = "https://sqs.us-east-1.amazonaws.com/570108184261/TheRushingRiver";
+    public const string QueueUrl = "RemovedForSecurityReasons";
 
 }
 
